8bdfc5ad8349808d676415a25a83e021973fb51b
Orah Kittrell <OrahK613@gmail.com>
2015-03-10 13:32:44 -0400

    Again, Hugh

--- ContosoUniversity/Global.asax.cs
+++ ContosoUniversity/Global.asax.cs
@@ -26,15 +26,15 @@ namespace ContosoUniversity
             DbInterception.Add(new SchoolInterceptorLogging());
 
             // Only run migrations if the setting is true
-            if (bool.Parse(ConfigurationManager.AppSettings["MigrateDatabaseToLatestVersion"]))
-            {
-                var configuration = new ContosoUniversity.Migrations.Configuration();
-                //configuration.TargetDatabase = new DbConnectionInfo(
-                //"Server=tcp:g74xl0join.database.windows.net,1433;Database=DevNet;User ID=OrahK613@g74xl0join;Password=OKeter613;Trusted_Connection=False;",
-                //"System.Data.SqlClient");
-                var migrator = new DbMigrator(configuration);
-                migrator.Update();
-            }
+            //if (bool.Parse(ConfigurationManager.AppSettings["MigrateDatabaseToLatestVersion"]))
+            //{
+            //    var configuration = new ContosoUniversity.Migrations.Configuration();
+            //    //configuration.TargetDatabase = new DbConnectionInfo(
+            //    //"Server=tcp:g74xl0join.database.windows.net,1433;Database=DevNet;User ID=OrahK613@g74xl0join;Password=OKeter613;Trusted_Connection=False;",
+            //    //"System.Data.SqlClient");
+            //    var migrator = new DbMigrator(configuration);
+            //    migrator.Update();
+            //}
 
 
         }
